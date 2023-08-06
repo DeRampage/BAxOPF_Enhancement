62e500812841c4a2b1f4dca1d2e52b6dc7ad9cd0
DeRampage <git@philipp-loebel.de>
2023-08-06 21:07:32 +0200

    BrushClear Update NewFiles

--- addons/brushClearing/config.cpp
+++ addons/brushClearing/config.cpp
@@ -6,23 +6,11 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_trenches"};
+        requiredAddons[] = {"ace_trenches", "ace_gestures"};
         author = "Ampersand";
         authors[] = {"Ampersand","Rampage"};
         url = "https://github.com/ampersand38/Brush-Clearing";
         VERSION_CONFIG;
-    };
-};
-
-class CfgFunctions {
-    class TF47 
-    {
-        class brushClearing 
-        {
-            file = "z\TF47\addons\brushClearing\functions";
-            class clearBrush;
-            class canClearBrush;
-        };
     };
 };
 
@@ -31,14 +19,14 @@ class CfgVehicles {
     class CAManBase: Man {
         class ACE_SelfActions {
             class ACE_Equipment {
-                class ClearBrush {
+                class GVAR(clearBrush) {
                     displayName = "Clear Grass/Bush";
-                    condition = "[_player] call TF47_fnc_canClearBrush";
-                    //wait a frame to handle "Do When releasing action menu key" option
-                    statement = "[{[] call TF47_fnc_clearBrush},[]] call CBA_fnc_execNextFrame";
+                    condition = QUOTE(_player call FUNC(canClearBrush));
+                    // Wait a frame to handle "Do When releasing action menu key" option
+                    statement = QUOTE([ARR_2({_this call FUNC(clearBrush)},_player)] call CBA_fnc_execNextFrame);
                     exceptions[] = {};
                     showDisabled = 0;
-                    icon = "z\TF47\addons\brushClearing\ui\bushremover.paa";
+                    icon = "\a3\modules_f\data\hideterrainobjects\icon32_ca.paa";
                 };
             };
         };
@@ -46,3 +34,4 @@ class CfgVehicles {
 };
 
 #include "CfgEventHandlers.hpp"
+#include "CfgVersioning.hpp"
