986dcf4755d0c118f5c491ca3d3b2d159d1d6df9
DeRampage <git@philipp-loebel.de>
2023-08-07 16:25:18 +0200

    finish it up plllllsssss

--- tools/stringtable_validator.py
+++ tools/stringtable_validator.py
@@ -53,7 +53,7 @@ def check_stringtable(filepath):
             print("  ERROR: Package name attribute '{}' is all lowercase, should be in titlecase.".format(package_name))
             errors += 1
 
-        if package_name.lower() != os.path.basename(os.path.dirname(filepath)):
+        if package_name != os.path.basename(os.path.dirname(filepath)):
             print("  ERROR: Package name attribute '{}' does not match the component folder name '{}'.".format(package_name,filepath))
             errors += 1
 
