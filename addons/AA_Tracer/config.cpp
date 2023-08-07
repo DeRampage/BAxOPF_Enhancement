d6631c683c784034775dd95d3611e505b01ea7f1
DeRampage <git@philipp-loebel.de>
2023-08-07 02:38:42 +0200

    clean

--- addons/AA_Tracer/config.cpp
+++ addons/AA_Tracer/config.cpp
@@ -2,32 +2,31 @@
 
 class CfgPatches
 {
-	class ADDON
-	{
-		name = COMPONENT;
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {"A3_Weapons_F","A3_Weapons_F_Jets"};
-		author[] = {"ANZACSAS Steven","Rampage"};
-		VERSION_CONFIG;
-	};
+    class ADDON
+    {
+        name = COMPONENT;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Weapons_F","A3_Weapons_F_Jets"};
+        author[] = {"ANZACSAS Steven","Rampage"};
+        VERSION_CONFIG;
+    };
 };
 class CfgAmmo
 {
-	class BulletBase;
-	class B_35mm_AA: BulletBase
-	{
-		brightness = 50;
-		timeToLive = 200;
-		tracerEndTime = 200;
-	};
-	class ammo_Gun35mmAABase;
-	class ammo_AAA_Gun35mm_AA: ammo_Gun35mmAABase
-	{
-		brightness = 50;
-		timeToLive = 200;
-		tracerEndTime = 200;
-	};
+    class BulletBase;
+    class B_35mm_AA: BulletBase
+    {
+        brightness = 50;
+        timeToLive = 200;
+        tracerEndTime = 200;
+    };
+    class ammo_Gun35mmAABase;
+    class ammo_AAA_Gun35mm_AA: ammo_Gun35mmAABase
+    {
+        brightness = 50;
+        timeToLive = 200;
+        tracerEndTime = 200;
+    };
 };
-
