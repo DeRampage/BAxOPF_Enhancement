b5e5e7f6de350cc212baadbf3215c6f52b08d58c
DeRampage <git@philipp-loebel.de>
2023-08-14 19:06:24 +0200

    fixed AttackHelo and added some Group Classes

--- mod.cpp
+++ mod.cpp
@@ -1,19 +1,19 @@
 /// Name of your mod
 name = "BAxOPF Enhancement";
 /// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
-picture = "tf47forecon.paa";
+picture = "47th_STS_color_512x512.paa";
 /// Display next to the item added by the mod
-logoSmall = "tf47forecon.paa";
+logoSmall = "47th_STS_color_512x512.paa";
 /// Logo displayed in the main menu
-logo = "tf47forecon.paa";
+logo = "47th_STS_color_512x512.paa";
 /// When the mouse is over, in the main menu
 logoOver = "tf47forecon.paa";
 /// Website URL, that can accessed from the expansions menu
-action = "https://steamcommunity.com/groups/tf47forecon";
+action = "https://steamcommunity.com/groups/47STS";
 // name of the action button
 actionName = "Find us on Steam";
 /// Tool tip displayed when the mouse is left over, in the main menu
-tooltipOwned = "BAxOPF on!";
+tooltipOwned = "Operate on!";
 /// Color used for DLC stripes and backgrounds (RGBA)
 dlcColor[] =
 {
